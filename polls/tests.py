fed88240d03afae0dc55e4b220947d1c21bb9cfd
Eric Abell <ericabell@users.noreply.github.com>
2017-10-16 16:08:49 -0500

    Write test for future questions and they aren't displayed on index page

--- polls/tests.py
+++ polls/tests.py
@@ -38,6 +38,16 @@ class QuestionModelTests(TestCase):
             ['<Question: Past question.>']
         )
 
+    def test_future_question(self):
+        """
+        Questions with a pub_date in the future aren't displayed on the index
+        page.
+        """
+        create_question(question_text="Future question.", days=30)
+        response = self.client.get(reverse('polls:index'))
+        self.assertContains(response, "No polls are available.")
+        self.assertQuerysetEqual(response.context['latest_question_list'], [])
+
     def test_was_published_recently_with_future_question(self):
         """
         was_published_recently() returns False for questions whose pub_date
