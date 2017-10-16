2d80dbff1f6559969e9b210a7e70a7068c6d4794
Eric Abell <ericabell@users.noreply.github.com>
2017-10-16 11:29:37 -0500

    Fixed some minor typos in the vote method for polls

--- polls/views.py
+++ polls/views.py
@@ -25,7 +25,7 @@ def results(request, question_id):
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     try:
-        selected_choice = question.choice_set.get(pk=requestion.POST['choice'])
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
     except (KeyError, Choice.DoesNotExist):
         # redisplay the question voting form.
         return render(request, 'polls/detail.html', {
@@ -36,4 +36,4 @@ def vote(request, question_id):
         selected_choice.votes += 1
         selected_choice.save()
 
-        return HttpResponseRedirect(reverse('pollsLresults', args=(question.id,)))
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
